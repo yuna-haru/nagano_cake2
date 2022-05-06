8079fe7959ec6871ee455641020623e45af3e5d1
yuna-haru <skywar.love22@gmail.com>
2022-05-06 12:31:50 +0000

    public/confirm

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,16 +6,12 @@ class Public::OrdersController < ApplicationController
 
 
   def confirm
-    @order = Order.new(order_params)
-    binding.pry
-    if params[:order][:order_address] == "0"
-　　　@order = current_customer
-    elsif params[:order][:order_address] == "1"
-      @order = Order.find(params[:id])
-      @order = Order.new
-    elsif params[:order][:order_address] == "2"
-      @order = Order.new
-    end
+  @cart_item= current_customer.cart_items
+  @order = Order.new(order_params)
+  @order.postal_code = current_customer.postal_code
+  @order.address = current_customer.address
+  @order.name = current_customer.first_name + current_customer.last_name
+  @oder.payment_method = current_customer.payment_method
   end
 
   def index
