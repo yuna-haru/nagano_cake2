5934129716cf09527b5c902c27211200a9789c48
yuna-haru <skywar.love22@gmail.com>
2022-05-07 12:50:16 +0000

    public/order/new again

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,12 +6,23 @@ class Public::OrdersController < ApplicationController
 
 
   def confirm
-  @cart_item= current_customer.cart_items
-  @order = Order.new(order_params)
-  @order.postal_code = current_customer.postal_code
-  @order.address = current_customer.address
-  @order.name = current_customer.first_name + current_customer.last_name
-  @oder.payment_method = current_customer.payment_method
+    @order = Order.new(order_params)
+    if params[:order][:order_address] == "0"
+　　  @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+    elsif params[:order][:order_address] == "1"
+      @order = Order.find(params[:id])
+    elsif params[:order][:order_address] == "2"
+      @order = Order.new
+    end
+#binding.pry
+
+    #@order.postal_code = current_customer.postal_code
+    #@order.address = current_customer.address
+    #@order.name = current_customer.first_name + current_customer.last_name
+  　#@oder.payment_method = current_customer.payment_method
+    #@cart_item= current_customer.cart_items
   end
 
   def index
