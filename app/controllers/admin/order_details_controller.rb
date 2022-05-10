fe0644342b03efab99d36f1267277f129484c050
yuna-haru <skywar.love22@gmail.com>
2022-05-10 02:24:26 +0000

    admin/orders/show totyuu

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -7,7 +7,7 @@ class Admin::OrderDetailsController < ApplicationController
         redirect_to admin_order_path(order_detail.order.id)
     end
 
-     private
+    private
 
   def order_detail_params
     params.require(:order_detail).permit(:order_detail_status)
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,13 +3,20 @@ class Admin::OrdersController < ApplicationController
   def show
     @customer = Customer.find(params[:id])
     @order = Order.find(params[:id])
-    #@order.cart_items = current_customer.cart_items
+    @order.order_details = OrderDetail.all
   end
 
   def update
    order = Order.find(params[:id])
    order.update(order_params)
-   redirect_to admin_orders_show_path
+   redirect_to admin_order_path(order.id)
+  end
+
+
+  private
+
+  def order_params
+    params.require(:order).permit(:order_status)
   end
 
 end
