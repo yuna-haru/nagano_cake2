e408c9eb67198b342be5fc1aae26e16a3a5f11af
yuna-haru <skywar.love22@gmail.com>
2022-05-11 12:53:45 +0000

    [start]注文ステータスと製作ステータスの連動

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -4,6 +4,17 @@ class Admin::OrderDetailsController < ApplicationController
         order_detail = OrderDetail.find(params[:id])
         order_status = params[:order_detail][:order_detail_status].to_i
         order_detail.update(order_detail_params)
+        
+        # 1.「 注文詳細の製作ステータスが製作中だったら」というif文
+        # 2. 注文詳細に紐づく注文データを取得
+        # 3. 注文データのステータスを「製作中」に更新
+        
+        # 1. 注文詳細に紐づく注文データを取得
+        # 2. 注文データに紐づく注文詳細すべてを取得
+        # 3. 注文詳細ひとつひとつ取り出して、「製作完了」以外が入っていないかを確認(難しい)
+        # 4. もしすべて製作完了だったら、注文ステータスを「発送準備中」に更新
+
+        
         redirect_to admin_order_path(order_detail.order.id)
     end
 
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -9,6 +9,15 @@ class Admin::OrdersController < ApplicationController
   def update
    order = Order.find(params[:id])
    order.update(order_params)
+   if order_status == "入金確認"
+    @order = @order.order_status.order_detail_status.all
+    @order.order_details.each |order_detail|
+      @order.order_detail = Or
+   end
+   # 1. 「注文statusが、「入金確認」だったら」というif文
+   # 2. 注文に紐づく注文詳細すべてを取得
+   # 3. 注文詳細ひとつひとつのデータを取り出して、製作ステータスを「製作待ち」に変更する
+   
    redirect_to admin_order_path(order.id)
   end
 
